d36fa2763db8b71b71b6d27d4af649f0e2f19f34
lazyimtiaz <imtiazuddin782001@gmail.com>
2025-09-19 11:53:21 +0600

    added all routes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        return view('pages.home');
+    }
+     public function about(){
+        return view('pages.about');
+    }
+     public function contact(){
+        return view('pages.contact');
+    }
 }
